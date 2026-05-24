30823d4a25dbd05d673ee2ad6bd9bb3cd05cd275
Manish Bisht <manishbisht77777@gmail.com>
2026-05-24 20:04:54 +0530

    upload sql

---
+++ project_sql/3_top_demanded_skils.sql
@@ -0,0 +1,16 @@
+
+
+SELECT 
+    skills,
+    COUNT(skills_job_dim.job_id) as demaand_skils
+FROM job_postings_fact
+
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+    job_title_short = 'Data Analyst'
+GROUP BY 
+    skills
+ORDER BY
+    demaand_skils DESC
+LIMIT 5;
